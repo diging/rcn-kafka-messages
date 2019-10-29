d1692c33902904acf3b977a2b51b02da3079ec21
Julia Damerow <julia.damerow@gmail.com>
2019-10-29 11:56:01 -0400

    renaming of upload topic

--- rcn-kafka-messages/src/main/java/edu/asu/diging/rcn/kafka/messages/KafkaTopics.java
+++ rcn-kafka-messages/src/main/java/edu/asu/diging/rcn/kafka/messages/KafkaTopics.java
@@ -2,8 +2,8 @@ package edu.asu.diging.rcn.kafka.messages;
 
 public interface KafkaTopics {
 
-    public final static String UPLOAD_DATASET_TOPIC = "_rcn_upload_dataset_topic";
-    public final static String UPLOAD_DATASET_STATUS_TOPIC = "_rcn_upload_dataset_status_topic";
+    public final static String UPLOAD_DATASET_TOPIC = "_rcn_import_dataset_topic";
+    public final static String UPLOAD_DATASET_STATUS_TOPIC = "_rcn_import_dataset_status_topic";
     
     public final static String MATCH_DATASETS_TOPIC = "_rcn_match_datasets_topic";
 }
